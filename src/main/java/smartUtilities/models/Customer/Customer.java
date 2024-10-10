2d481e2fe444c733e8efd7cb526da833217a2841
FrotaLucas <lucasdiasfrota@gmail.com>
2024-10-10 16:26:08 +0200

    implement get and set for Class Customer

--- src/main/java/smartUtilities/models/Customer/Customer.java
+++ src/main/java/smartUtilities/models/Customer/Customer.java
@@ -5,51 +5,55 @@ import smartUtilities.enums.Gender;
 
 public class Customer implements ICustomer {
 
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate birthDate;
+
+    private Gender gender;
+
     @Override
     public String getFirstName() {
-        // TODO Auto-generated method stub
-        return null;
+        
+        return firstName;
     }
     
     @Override
     public String getLastName() {
-        // TODO Auto-generated method stub
-        return null;
+        return lastName;
     }
    
     @Override
     public LocalDate getBirthDate() {
-        // TODO Auto-generated method stub
-        return null;
+        return birthDate;
     }
 
     @Override
     public Gender getGender() {
-        // TODO Auto-generated method stub
-        return null;
+        return gender;
     }
 
     @Override
     public void setFirstName(String firstName) {
-        // TODO Auto-generated method stub
-        
+        this.firstName = firstName;
     }
 
     @Override
     public void setLastName(String lastName) {
-        // TODO Auto-generated method stub
+        this.lastName = lastName;
             
     }
         
     @Override
     public void setBirthDate(LocalDate birtDate) {
-        // TODO Auto-generated method stub
+       this.birthDate = birtDate;
     }
 
 
     @Override
     public void setGender(Gender gender) {
-        // TODO Auto-generated method stub
+       this.gender = gender;
         
     }
 }
