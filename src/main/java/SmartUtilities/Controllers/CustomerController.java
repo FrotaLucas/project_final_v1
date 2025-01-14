72e8bea378ac79e8e2f9c4497395f71258a116b8
FrotaLucas <lucasdiasfrota@gmail.com>
2025-01-14 14:00:00 +0100

    format code

--- src/main/java/SmartUtilities/Controllers/CustomerController.java
+++ src/main/java/SmartUtilities/Controllers/CustomerController.java
@@ -11,7 +11,6 @@ import SmartUtilities.Model.Customer.Customer;
 import SmartUtilities.Services.CustomerService.CustomerService;
 import org.glassfish.jersey.internal.inject.Custom;
 
-
 import java.io.IOException;
 
 import java.io.Reader;
@@ -28,32 +27,31 @@ import java.util.Map;
 @Path("api/customers")
 public class CustomerController {
 
-    //option 1
+    // option 1
     private Database database = new Database();
     private CustomerService customerService = new CustomerService(database);
 
-
-    //option 2
-//    private final ICustomerService customerService;
-//
-//    @Inject
-//    public CustomerController(ICustomerService costumerService)
-//    {
-//        this.customerService = costumerService;
-//    }
+    // option 2
+    // private final ICustomerService customerService;
+    //
+    // @Inject
+    // public CustomerController(ICustomerService costumerService)
+    // {
+    // this.customerService = costumerService;
+    // }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response addCustomer(Customer customer) {
-        //corrigir metod customerService para boleano
-        //if(customer == null || !customerService.addNewCustomer(customer))
-        if(customer == null)
-            {return Response.status(Response.Status.BAD_REQUEST)
-                .entity("No customer to add.")
-                .build();
-            }
-        
+        // corrigir metod customerService para boleano
+        // if(customer == null || !customerService.addNewCustomer(customer))
+        if (customer == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("No customer to add.")
+                    .build();
+        }
+
         customerService.addNewCustomer(customer);
         return Response.status(Response.Status.CREATED).entity(customer).build();
 
@@ -61,16 +59,14 @@ public class CustomerController {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getCustomers()
-    {
+    public Response getCustomers() {
         List<Customer> customers = customerService.getCustomers();
-        if(customers != null)
-        {
+        if (customers != null) {
             return Response.ok(customers).build();
         }
         return Response.status(Response.Status.NOT_FOUND)
-        .entity("Customers not found")
-        .build();
+                .entity("Customers not found")
+                .build();
 
     }
 
@@ -83,9 +79,9 @@ public class CustomerController {
                     .entity("Customer with id null.")
                     .build();
         }
-    
+
         Customer dbCustomer = customerService.getCustomerByUuid(id);
-    
+
         if (dbCustomer != null) {
             Map<String, Object> dbCustomerProperties = new LinkedHashMap<>();
             dbCustomerProperties.put("id", dbCustomer.getId());
@@ -95,47 +91,42 @@ public class CustomerController {
             dbCustomerProperties.put("gender", dbCustomer.getGender());
 
             Map<String, Object> serviceResponseProperties = Map
-            .of("customer", Map
-                .of("type", "object",
-                "required", List.of("firstName","lastName","gender"),
-                "properties",dbCustomerProperties));
+                    .of("customer", Map
+                            .of("type", "object",
+                                    "required", List.of("firstName", "lastName", "gender"),
+                                    "properties", dbCustomerProperties));
 
-            ServiceResponse<Customer> serviceResponse = new ServiceResponse<> (
+            ServiceResponse<Customer> serviceResponse = new ServiceResponse<>(
                     "Customer-JSON-Schema",
                     "object",
                     "customer",
-                    serviceResponseProperties
-            );
-    
+                    serviceResponseProperties);
+
             return Response.ok(serviceResponse).build();
         }
-    
+
         return Response.status(Response.Status.NOT_FOUND)
                 .entity("Customer with id " + id + " not found.")
                 .build();
     }
 
-
     @DELETE
     @Path("/{id}")
-    public Response deleteCustomer(@PathParam("id") String id)
-    {
-        if (id == null)
-         {
+    public Response deleteCustomer(@PathParam("id") String id) {
+        if (id == null) {
             return Response.status(Response.Status.BAD_REQUEST)
-                .entity("Id cannont be null")
-                .build(); }
-            
+                    .entity("Id cannont be null")
+                    .build();
+        }
+
         boolean isDeleted = customerService.deleteCustomering(id);
-        if( !isDeleted)
-        {
+        if (!isDeleted) {
             return Response.status(Response.Status.NOT_FOUND)
-                .entity("Customer with Id" + id + "not found")
-                .build();
-             
+                    .entity("Customer with Id" + id + "not found")
+                    .build();
+
         }
         return Response.status(Response.Status.NO_CONTENT).build();
     }
-
 
 }
