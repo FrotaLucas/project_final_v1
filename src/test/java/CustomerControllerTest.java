cea6aa123e87e8ad60907cc4c636f280d0cec041
FrotaLucas <lucasdiasfrota@gmail.com>
2025-01-21 16:08:49 +0100

    install dependency rest-assured

--- src/test/java/CustomerControllerTest.java
+++ src/test/java/CustomerControllerTest.java
@@ -1,2 +1,15 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import static io.restassured.RestAssured.*;
+
 public class CustomerControllerTest {
+
+    private static final String BASE_URI = "http://localhost:4080/api/customers";
+
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = BASE_URI;
+    }
+
+    // You can now write test methods here
 }
